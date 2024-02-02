0f4363edc1d9e81953f188f316d3aa7efbf64e59
Ango <ahmed.ango@gmail.com>
2024-02-02 09:47:29 -0600

    main

--- main.tf
+++ main.tf
@@ -1,13 +1,32 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
-  }
+ resource "aws_lightsail_instance" "server1" {
+  name = "dev-server"
+  blueprint_id = "ubuntu_18_04"
+  bundle_id = "medium_1_0"
+  availability_zone = "us-east-1a"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              echo '<h1>This is deployed by Serge </h1>' | sudo tee /var/www/html/index.html
+              EOF
+}
+  
+*** output.tf
+
+output "pip" {
+  value = aws_lightsail_instance.server1.public_ip_address
 }
 
-provider "aws" {
-  
-  region = "us-east-1"
+output "privip" {
+  value = aws_lightsail_instance.server1.private_ip_address
 }
+
+ git add .
+git commit -m "lightsail"
+git push origin ticket1
+
+*** terraform default gitignore file
+
+https://github.com/github/gitignore/blob/main/Terraform.gitignore
