2888e109a4a156b7ca8ee70dc92f610ab7437b86
Ango <ahmed.ango@gmail.com>
2024-02-02 10:05:38 -0600

    lightsal

--- main.tf
+++ main.tf
@@ -9,24 +9,6 @@
               sudo apt-get install -y apache2
               sudo systemctl start apache2
               sudo systemctl enable apache2
-              echo '<h1>This is deployed by Serge </h1>' | sudo tee /var/www/html/index.html
+              echo '<h1>This is deployed by Mr. Ango </h1>' | sudo tee /var/www/html/index.html
               EOF
 }
-  
-*** output.tf
-
-output "pip" {
-  value = aws_lightsail_instance.server1.public_ip_address
-}
-
-output "privip" {
-  value = aws_lightsail_instance.server1.private_ip_address
-}
-
- git add .
-git commit -m "lightsail"
-git push origin ticket1
-
-*** terraform default gitignore file
-
-https://github.com/github/gitignore/blob/main/Terraform.gitignore
